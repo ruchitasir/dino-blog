932384ef044d0100c52afe46ab00f0e80b64c4c2
Ruchita Sirkanungo <ruchitas@Ruchitas-MacBook-Pro.local>
2020-05-05 12:25:14 -0700

    done with prompt and input field button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React , { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Comment from './Comment'
 
 
 function App(props) {
+  const DEFAULT_BODY = props.post.body
+  let [body,setBody]= useState(DEFAULT_BODY);
+
   let formattedComments = props.post.comments.map((c,i)=>{
     return <Comment text={c} key={i}/>
   })
@@ -12,6 +15,11 @@ function App(props) {
     for(let i=0;i<props.post.comments.length;i++){
       commentsLoop.push(<Comment text={props.post.comments[i]} />)
     }
+  const changeBody=()=>{
+   // body = prompt("What should the new body be?");
+      body = document.getElementById('bodyId').value
+     setBody(body)
+  }
 
   return (
     <div className="App">
@@ -19,13 +27,24 @@ function App(props) {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>{props.post.title}</h1>
         <p> by {props.post.author}</p>
-        <p> {props.post.body}</p>
+      </header>
+       
+        <p> Body : {body}</p>
+
+        <input type="text" placeholder="edit the body here" name="body" id="bodyId"/>
+        
+        <button onClick={changeBody}> Edit Body
+        </button>
+
+        <hr />
+
         <h2>Comments: </h2>
         <p>{props.post.comments[0]}</p>
         <p>{props.post.comments[1]} </p>
         <p>{props.post.comments[2]} </p>
-       </header>
-       
+      
+        <hr />
+
         <h2>Comments as a component:</h2>
         <Comment text={props.post.comments[0]} />
         <Comment text={props.post.comments[1]} />
